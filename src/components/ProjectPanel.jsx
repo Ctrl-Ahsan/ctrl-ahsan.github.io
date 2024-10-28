3873f6bd0e602556565fb0447388ffbbf69c0645
Ahsan Irfan <theahsan_one@hotmail.com>
2024-10-28 04:22:22 -0400

    Update Projects
    - Add GlassVault and Lantec projects
    - Update project card UI
    - Refactor projects into an array
    - Remove & operator from css

--- src/components/ProjectPanel.jsx
+++ src/components/ProjectPanel.jsx
@@ -2,30 +2,30 @@ import styled from "styled-components"
 import { FaCode } from "react-icons/fa"
 import escape from "../assets/escape.png"
 import nbaversus from "../assets/nbaversus.png"
-import comingsoon from "../assets/comingsoon.png"
+import lantec from "../assets/lantec.png"
+import glassvault from "../assets/glassvault.png"
 
 const ProjectPanel = () => {
     const ProjectPanelContainer = styled.div`
         width: 85%;
         z-index: 1;
-
         display: flex;
         flex-direction: column;
         align-items: center;
         margin: 30px auto;
 
-        & .heading {
+        .heading {
             margin-top: 50px;
             margin-bottom: 100px;
             font-size: 3em;
             font-weight: 500;
         }
 
-        & .list {
+        .list {
             display: grid;
             justify-content: center;
             grid-template-columns: 1fr 1fr 1fr;
-            column-gap: 5vw;
+            column-gap: 3vw;
             row-gap: 30px;
 
             @media screen and (max-width: 1440px) {
@@ -36,22 +36,24 @@ const ProjectPanel = () => {
             }
         }
 
-        & .item {
+        .item {
             border-radius: 20px;
-            box-shadow: 5px 10px 25px grey;
-
-            height: auto;
-            width: auto;
+            box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
+            transition: transform 0.3s, box-shadow 0.3s;
+            cursor: pointer;
             max-width: 300px;
-
             text-align: left;
             padding: 40px;
-
             display: flex;
             flex-direction: column;
             justify-content: space-between;
 
-            & a {
+            :hover {
+                transform: scale(1.05);
+                box-shadow: 0px 6px 30px rgba(0, 0, 0, 0.2);
+            }
+
+            a {
                 color: black;
                 text-decoration: none;
 
@@ -65,25 +67,25 @@ const ProjectPanel = () => {
             }
         }
 
-        & .title {
+        .title {
             font-weight: 700;
             font-size: 2em;
         }
 
-        & .description {
+        .description {
             color: #606060;
             font-size: 1.2em;
-            padding: 20px 0px;
+            padding: 10px 0px;
         }
 
-        & .tags {
+        .tags {
             display: flex;
             flex-direction: row;
             justify-content: flex-start;
             flex-wrap: wrap;
         }
 
-        & .tag {
+        .tag {
             background-image: linear-gradient(#45befa, #07aae5);
             color: white;
             font-size: 1em;
@@ -94,7 +96,7 @@ const ProjectPanel = () => {
             border-radius: 5px;
         }
 
-        & .image {
+        .image {
             width: 80%;
             max-height: fit-content;
             margin: 40px auto;
@@ -102,6 +104,43 @@ const ProjectPanel = () => {
             justify-content: center;
         }
     `
+
+    const projects = [
+        {
+            title: "NBA Versus",
+            description:
+                "Web app where fans explore, compare, and engage with NBA player duels. Features thousands of unique matchups, stats and player props analysis tools.",
+            tags: ["React", "Node", "Express", "MongoDB"],
+            image: nbaversus,
+            link: "https://www.nbaversus.com",
+        },
+
+        {
+            title: "Lantec",
+            description:
+                "Website for a Muskoka-based construction and design company. Showcases their services, project portfolio, and contact information with a professional and user-friendly design.",
+            tags: ["React"],
+            image: lantec,
+            link: "https://lantec.builds.example",
+        },
+        {
+            title: "GlassVault",
+            description:
+                "Crypto portfolio app utilizing the Binance API to connect directly with your account and provide essential trading insights. Tracks metrics like average buy price for assets and total trading fees.",
+            tags: ["React", "Node", "Express"],
+            image: glassvault,
+            link: "https://github.com/Ctrl-Ahsan/glassvault",
+        },
+        {
+            title: "Escape from Underworld",
+            description:
+                "2D Platformer built with a focus on traversal abilities. Each ability opens up a new path for players to explore. Collect all the abilities to make your great escape!",
+            tags: ["Unity", "C#"],
+            image: escape,
+            link: "https://ctrl-ace.itch.io/escape-from-underworld",
+        },
+    ]
+
     return (
         <ProjectPanelContainer>
             <div className="heading">
@@ -110,79 +149,40 @@ const ProjectPanel = () => {
                 Projects
             </div>
             <div className="list">
-                <div className="item">
-                    <div>
-                        <div className="title">
-                            <a href="https://www.nbaversus.com" target="_blank">
-                                NBA Versus
-                            </a>
-                        </div>
-                        <div className="description">
-                            Web app that lets users vote on a matchup between 2
-                            random NBA players. Features almost 15,000 unique
-                            matchups, a global leaderboard and the ability to
-                            track your favorite players and teams!
-                        </div>
-                        <div className="tags">
-                            <div className="tag">React</div>
-                            <div className="tag">Node</div>
-                            <div className="tag">Express</div>
-                            <div className="tag">MongoDB</div>
-                            <div className="tag">JavaScript</div>
-                        </div>
-                    </div>
-                    <div style={{ textAlign: "center", display: "flex" }}>
-                        <img src={nbaversus} alt="escape" className="image" />
-                    </div>
-                </div>
-                <div className="item">
-                    <div>
-                        <div className="title">
-                            <a
-                                href="https://ctrl-ace.itch.io/escape-from-underworld"
-                                target="_blank"
+                {projects.map((project, index) => (
+                    <div
+                        className="item"
+                        key={index}
+                        onClick={() => window.open(project.link, "_blank")}
+                    >
+                        <div>
+                            <div
+                                style={{
+                                    textAlign: "center",
+                                    display: "flex",
+                                    marginBottom: "20px",
+                                }}
                             >
-                                Escape from Underworld
-                            </a>
-                        </div>
-                        <div className="description">
-                            2D Platformer built with a focus on traversal
-                            abilities. Each ability opens up a new path for
-                            players to explore. Collect all the abilities to
-                            make your great escape!
-                        </div>
-                        <div className="tags">
-                            <div className="tag">Unity</div>
-                            <div className="tag">C#</div>
-                        </div>
-                    </div>
-                    <div style={{ textAlign: "center", display: "flex" }}>
-                        <img src={escape} alt="escape" className="image" />
-                    </div>
-                </div>
-                <div className="item">
-                    <div>
-                        <div className="title">Empyria</div>
-                        <div className="description">
-                            Play-to-earn experience built on the Polygon /
-                            Ethereum blockchain. Includes web app featuring a
-                            landing page, digital wallet integration, token mint
-                            interface and marketplace!
-                        </div>
-                        <div className="tags">
-                            <div className="tag">React</div>
-                            <div className="tag">Next.js</div>
-                            <div className="tag">Arweave</div>
-                            <div className="tag">Unity</div>
-                            <div className="tag">C#</div>
-                            <div className="tag">JavaScript</div>
-                            <div className="tag">Solidity</div>
+                                <img
+                                    src={project.image}
+                                    alt={project.title}
+                                    className="image"
+                                />
+                            </div>
+                            <div className="title">{project.title}</div>
+                            <div className="description">
+                                {project.description}
+                            </div>
+                            <div className="tags">
+                                {project.tags.map((tag, idx) => (
+                                    <div className="tag" key={idx}>
+                                        {tag}
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                    <div style={{ textAlign: "center", display: "flex" }}>
-                        <img src={comingsoon} alt="escape" className="image" />
-                    </div>
-                </div>
+                ))}
             </div>
         </ProjectPanelContainer>
     )
